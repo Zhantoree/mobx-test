2a0ec935f9d383e4cd37a6e6589d20a3406e138d
unknown <drug2334@gmail.com>
2024-07-03 22:40:40 +0500

    pushka nomer dva

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>PUSHKA</h1>
+      <h1>PUSHKA NOMER DVA</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
