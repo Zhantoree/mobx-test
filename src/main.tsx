f9b03778cb4ce4e31cb46a7ee8788dfdc05aa7c8
Zhantoree <97701154+Zhantoree@users.noreply.github.com>
2024-07-03 23:20:09 +0500

    changed main from github 23:19

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-console.log('change from webstorm')
+console.log('changed main from github 23:19')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
